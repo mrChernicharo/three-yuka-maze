ae171ae728e1b8c7a3907b6faf723f7e6695fe92
Felipe Chernicharo <Felipe.Melo_Chernicharo.genesys.com>
2023-12-12 23:32:52 -0300

    render gltf map and navMesh

--- main.js
+++ main.js
@@ -5,40 +5,72 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 console.log({ THREE });
 
-const glTFLoader = new GLTFLoader();
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x232323);
-document.body.appendChild(renderer.domElement);
-const clock = new THREE.Clock();
-const scene = new THREE.Scene();
-const ambientLight = new THREE.AmbientLight(0xffffdd, 200);
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
-camera.position.set(-4, 10, 20);
-const orbit = new OrbitControls(camera, renderer.domElement);
-scene.add(camera);
-scene.add(ambientLight);
-const mouseRay = new THREE.Raycaster();
+let glTFLoader;
+let renderer;
+let clock;
+let scene;
+let ambientLight;
+let camera;
+let orbit;
+let mouseRay;
 
-const gridHelper = new THREE.GridHelper(12, 12);
-scene.add(gridHelper);
+function init() {
+  glTFLoader = new GLTFLoader();
 
-const axesHelper = new THREE.AxesHelper(12);
-scene.add(axesHelper);
-
-window.addEventListener("resize", () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+  renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
-});
+  renderer.setClearColor(0x232323);
+  document.body.appendChild(renderer.domElement);
+  clock = new THREE.Clock();
+  scene = new THREE.Scene();
+  ambientLight = new THREE.AmbientLight(0xffffdd, 200);
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
+  camera.position.set(-4, 10, 20);
+  orbit = new OrbitControls(camera, renderer.domElement);
+  scene.add(camera);
+  scene.add(ambientLight);
+  mouseRay = new THREE.Raycaster();
+}
 
-window.addEventListener("click", (e) => {
-  const mouseClickPos = new THREE.Vector2();
-  mouseClickPos.x = (e.clientX / window.innerWidth) * 2 - 1;
-  mouseClickPos.y = -(e.clientY / window.innerHeight) * 2 + 1;
+async function setupWorldObjects() {
+  // const gridHelper = new THREE.GridHelper(12, 12);
+  // scene.add(gridHelper);
 
-  mouseRay.setFromCamera(mouseClickPos, camera);
-});
+  // const axesHelper = new THREE.AxesHelper(12);
+  // scene.add(axesHelper);
+
+  const glb = await glTFLoader.loadAsync("/assets/MAZE.glb");
+  const stoneTexture = await new THREE.TextureLoader().loadAsync("/assets/stoneTexture.jpeg");
+
+  const [levelMap, navMesh] = glb.scene.children;
+  const mapTexture = stoneTexture.clone();
+  mapTexture.wrapS = THREE.RepeatWrapping;
+  mapTexture.wrapT = THREE.RepeatWrapping;
+  mapTexture.repeat.set(25, 25);
+  levelMap.material = new THREE.MeshBasicMaterial({ color: 0x6868686, map: mapTexture });
+  navMesh.material = new THREE.MeshBasicMaterial({ transparent: true, color: 0x884444, opacity: 0.2 });
+
+  scene.add(levelMap);
+  scene.add(navMesh);
+
+  console.log({ glb, levelMap, navMesh });
+}
+
+function setupDomListeners() {
+  window.addEventListener("resize", () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  });
+
+  window.addEventListener("click", (e) => {
+    const mouseClickPos = new THREE.Vector2();
+    mouseClickPos.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouseClickPos.y = -(e.clientY / window.innerHeight) * 2 + 1;
+
+    mouseRay.setFromCamera(mouseClickPos, camera);
+  });
+}
 
 function animate() {
   orbit.update();
@@ -47,6 +79,12 @@ function animate() {
 }
 
 function main() {
+  init();
+
+  setupDomListeners();
+
+  setupWorldObjects();
+
   requestAnimationFrame(animate);
 }
 
