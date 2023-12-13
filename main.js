4b1d7799d1b7e263e1ea62d42b7d3c320baf1627
Felipe Chernicharo <Felipe.Melo_Chernicharo.genesys.com>
2023-12-13 08:52:55 -0300

    entities moving

--- main.js
+++ main.js
@@ -12,24 +12,28 @@ let glTFLoader;
 let navMeshLoader;
 let renderer;
 let clock;
-let yukaTime;
+let time;
 let scene;
 let ambientLight;
 let camera;
 let orbit;
 let mouseRay;
 let entityManager;
+let navMesh;
 let graphHelper;
+let navMeshHelper; // convexRegionHelper
+let agentVehicles = [];
+let agentMeshes = [];
 
 const agentBlueprints = [
   { speed: 10, pos: new THREE.Vector3(19, 1.5, 18.5), color: 0x00ff00, spawnAt: 0 },
-  { speed: 8, pos: new THREE.Vector3(19, 1.5, 17), color: 0xff0000, spawnAt: 2 },
-  { speed: 10, pos: new THREE.Vector3(17, 1.5, 18.5), color: 0x00ff00, spawnAt: 4 },
-  { speed: 10, pos: new THREE.Vector3(16, 1.5, 18.5), color: 0x00ff00, spawnAt: 6 },
-  { speed: 10, pos: new THREE.Vector3(16, 1.5, 17), color: 0x00ff00, spawnAt: 8 },
-  { speed: 8, pos: new THREE.Vector3(18.5, 1.5, 16), color: 0xff0000, spawnAt: 10 },
-  { speed: 10, pos: new THREE.Vector3(17, 1.5, 17), color: 0x00ff00, spawnAt: 12 },
-  // { speed: 5, pos: new THREE.Vector3(-18, 1.5, 18), color: 0xff0000, spawnAt: 2 },
+  // { speed: 8, pos: new THREE.Vector3(19, 1.5, 17), color: 0xff0000, spawnAt: 2 },
+  // { speed: 10, pos: new THREE.Vector3(17, 1.5, 18.5), color: 0x00ff00, spawnAt: 4 },
+  // { speed: 10, pos: new THREE.Vector3(16, 1.5, 18.5), color: 0x00ff00, spawnAt: 6 },
+  // { speed: 10, pos: new THREE.Vector3(16, 1.5, 17), color: 0x00ff00, spawnAt: 8 },
+  // { speed: 8, pos: new THREE.Vector3(18.5, 1.5, 16), color: 0xff0000, spawnAt: 10 },
+  // { speed: 10, pos: new THREE.Vector3(17, 1.5, 17), color: 0x00ff00, spawnAt: 12 },
+  { speed: 5, pos: new THREE.Vector3(-18, 1.5, 18), color: 0xff0000, spawnAt: 2 },
   // { speed: 20, pos: new THREE.Vector3(-18, 1.5, -18), color: 0x0000ff, spawnAt: 5 },
 ];
 
@@ -48,10 +52,9 @@ function init() {
   orbit = new OrbitControls(camera, renderer.domElement);
   scene.add(camera);
   scene.add(ambientLight);
-  mouseRay = new THREE.Raycaster();
 
   entityManager = new YUKA.EntityManager();
-  yukaTime = new YUKA.Time();
+  time = new YUKA.Time();
 }
 
 async function drawMapAndNavMesh() {
@@ -76,62 +79,56 @@ async function drawMapAndNavMesh() {
 
   scene.add(levelMap);
 
-  const navMesh = await navMeshLoader.load("/assets/MAZE-NavMesh.glb");
+  navMesh = await navMeshLoader.load("/assets/MAZE-NavMesh.glb");
+
   const graph = navMesh.graph;
-  const graphHelper = createGraphHelper(graph, 0.2);
+  graphHelper = createGraphHelper(graph, 0.2);
   scene.add(graphHelper);
 
-  console.log(navMeshLoader, navMesh);
+  navMeshHelper = createConvexRegionHelper(navMesh);
+  scene.add(navMeshHelper);
 
-  const convexRegHelper = createConvexRegionHelper(navMesh);
-  scene.add(convexRegHelper);
-
-  console.log({ glb, levelMap });
+  console.log({ glb, levelMap, navMeshLoader, navMesh });
 }
 
 function drawAgents() {
+  const agentRadius = 0.25;
+  const agentHeight = 1;
+  const agentGeometry = new THREE.CylinderGeometry(agentRadius, 0, agentHeight);
+
   for (let i = 0; i < agentBlueprints.length; i++) {
     const agent = agentBlueprints[i];
-    const agentRadius = 0.25;
-    const agentHeight = 1;
-    const agentGeometry = new THREE.CylinderGeometry(agentRadius, 0, agentHeight);
     const agentMaterial = new THREE.MeshBasicMaterial({ color: agent.color });
     const agentMesh = new THREE.Mesh(agentGeometry, agentMaterial);
+    agentMesh.name = "Agent Mesh";
     agentMesh.position.set(agent.pos.x, agent.pos.y, agent.pos.z);
-    agentMesh.lookAt(new THREE.Vector3(0, 1, 0));
 
     const agentVehicle = new YUKA.Vehicle();
+    agentVehicle.name = "Agent Vehicle";
     agentVehicle.boundingRadius = agentGeometry.boundingSphere;
     agentVehicle.maxSpeed = 12;
     agentVehicle.mass = 0.5;
     agentVehicle.setRenderComponent(agentMesh, (entity, renderComponent) => {
       renderComponent.matrix.copy(entity.worldMatrix);
     });
-    // agentVehicle.position.copy(path.current());
-    agentVehicle.rotation.copy(new Quaternion(agentMesh.quaternion));
+    agentVehicle.position.copy(agentMesh.position);
 
     const followPathBehavior = new YUKA.FollowPathBehavior();
     followPathBehavior.active = false;
-    followPathBehavior.nextWaypointDistance = 0.5;
-
+    followPathBehavior.nextWaypointDistance = 1;
     agentVehicle.steering.add(followPathBehavior);
 
-    // agentVehicle.maxSpeed = THREE.MathUtils.lerp(2, 8.5, Math.random());
-    // agentVehicle.mass = THREE.MathUtils.lerp(0.1, 1.2, Math.random());
-    agentVehicle.name = `mass ${agentVehicle.mass} maxSpeed ${agentVehicle.maxSpeed}`;
-    console.log(agentVehicle.name);
-
+    scene.add(agentMesh);
     entityManager.add(agentVehicle);
-
-    setTimeout(() => {
-      scene.add(agentMesh);
-    }, agent.spawnAt * 1000);
+    agentMeshes.push(agentMesh);
+    agentVehicles.push(agentVehicle);
   }
 }
 
 function setupDomListeners() {
   window.addEventListener("resize", () => {
     camera.aspect = window.innerWidth / window.innerHeight;
+    console.log(camera);
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
   });
@@ -141,18 +138,52 @@ function setupDomListeners() {
     mouseClickPos.x = (e.clientX / window.innerWidth) * 2 - 1;
     mouseClickPos.y = -(e.clientY / window.innerHeight) * 2 + 1;
 
+    mouseRay = new THREE.Raycaster();
     mouseRay.setFromCamera(mouseClickPos, camera);
 
-    console.log(mouseRay);
+    const intersects = mouseRay.intersectObject(navMeshHelper);
+
+    console.log(intersects);
+
+    if (intersects.length > 0) {
+      entityManager.entities.forEach((entity, i) => {
+        const agentVehicle = agentVehicles[i];
+        const path = findPathTo(agentVehicle.position, new YUKA.Vector3().copy(intersects[0].point));
+
+        const followPathBehavior = agentVehicle.steering.behaviors[0];
+        followPathBehavior.path.clear();
+        followPathBehavior.active = true;
+        for (const point of path) {
+          followPathBehavior.path.add(point);
+        }
+        console.log(agentVehicle, path, followPathBehavior);
+      });
+
+      function findPathTo(from, to) {
+        return navMesh.findPath(from, to);
+      }
+    }
   });
 }
 
 function animate() {
   orbit.update();
-  const delta = yukaTime.update().getDelta();
+
+  // agentVehicles.forEach((v) => {
+  // console.log(v.steering.behaviors[0]);
+  // v.position.copy(followPathBehavior.path.current());
+  // });
+  // console.log(agentVehicles[0].position);
+  agentVehicles.forEach((v, i) => {
+    agentMeshes[i].position.copy(v.position);
+    // agentVehicle.position.copy(path.current());
+    // agentVehicle.rotation.copy(new Quaternion(agentMesh.quaternion));
+  });
+  const delta = time.update().getDelta();
   entityManager.update(delta);
   renderer.render(scene, camera);
-  requestAnimationFrame(animate);
+
+  // requestAnimationFrame(animate);
 }
 
 function main() {
@@ -164,7 +195,8 @@ function main() {
 
   drawAgents();
 
-  requestAnimationFrame(animate);
+  // requestAnimationFrame(animate);
+  renderer.setAnimationLoop(animate);
 }
 
 main();
